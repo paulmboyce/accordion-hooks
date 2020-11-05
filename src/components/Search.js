3bc5cf98bf9f45afdff1cebaecebeb606152fc72
paulmboyce <paul.boyce@goobito.com>
2020-11-05 16:27:47 -0300

    feat(search): click to get and open wikipedia page
    implementation of 2 clicks same button requires a reset of state and hence more rendering. there is
    a better way...

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,11 @@ import Axios from "axios";
 
 const Search = () => {
 	const [term, setTerm] = useState("cats");
+	const [pageId, setPageId] = useState("");
 	const [results, setResults] = useState([]);
+	const [fullUrl, setFullUrl] = useState("");
 
+	console.log("----> RENDER <---------------------");
 	useEffect(() => {
 		const doSearch = async () => {
 			const { data } = await Axios.get(
@@ -26,9 +29,53 @@ const Search = () => {
 		doSearch();
 	}, [term]);
 
+	useEffect(() => {
+		const getPage = async () => {
+			const { data } = await Axios.get(
+				"https://en.wikipedia.org/w/api.php",
+				{
+					params: {
+						action: "query",
+						prop: "info",
+						pageids: pageId,
+						inprop: "url",
+						format: "json",
+						origin: "*",
+					},
+				}
+			);
+			console.log(data.query.pages[pageId].fullurl);
+			setFullUrl(data.query.pages[pageId].fullurl);
+			console.log(pageId);
+		};
+
+		if (pageId !== "") {
+			getPage();
+			setPageId(""); // Important to reset these to be abel to click same item.
+		}
+	}, [pageId]);
+
+	useEffect(() => {
+		if (fullUrl !== "") {
+			console.log(`About to open: ${fullUrl}..`);
+			window.open(fullUrl);
+			setFullUrl("");
+		}
+	}, [fullUrl]);
+
 	const renderResults = results.map((result) => {
 		return (
 			<div className='item' key={result.pageid}>
+				<div className='right floated content'>
+					<div
+						onClick={(e) => {
+							setPageId(result.pageid);
+						}}
+						className='ui button blue'
+					>
+						Wikipedia
+					</div>
+				</div>
 				<div className='content'>
 					<div className='header'>{result.title}</div>
 					<div className='description'>
@@ -56,7 +103,9 @@ const Search = () => {
 					></input>
 				</div>
 			</div>
-			<div className='ui celled list'> {renderResults}</div>
+			<div className='ui middle aligned divided list'>
+				{renderResults}
+			</div>
 		</div>
 	);
 };
