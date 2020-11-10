1d2adfea73939773efcea0256cf1d2442b694002
paulmboyce <paul.boyce@goobito.com>
2020-11-10 12:29:07 -0300

    feat(dropdown): new dropdown <select> component
    Takes array of options, including a 'PLease select' asneeded

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
+import Dropdown from "./Dropdown";
 
 const items = [
 	{ title: "What is React?", content: "A way to get paid" },
@@ -13,14 +14,52 @@ const items = [
 		content: "Because it is popular and effective",
 	},
 ];
+const options = [
+	{ value: "red", label: "Red Face" },
+	{ value: "blue", label: "Blue Fist" },
+	{ value: "green", label: "Green Eyes" },
+];
+
+const optionsSize = [
+	{ value: "", label: "-- Please choose a size: --" },
+	{ value: "sm", label: "Teeny Small" },
+	{ value: "md", label: "Midroad Medium" },
+	{ value: "lg", label: "Big Style Large" },
+];
 
 const App = () => {
+	const initColor = 1;
+	const [color, setColor] = useState(options[initColor]);
+	const [size, setSize] = useState(optionsSize[0]);
+
+	useEffect(() => {
+		console.log(
+			`APP: selected color: ${JSON.stringify(color)}`
+		);
+	}, [color]);
+	useEffect(() => {
+		console.log(
+			`APP: selected size: ${JSON.stringify(size)}`
+		);
+	}, [size]);
+
 	return (
 		<div
 			className='ui container'
 			style={{ marginTop: "40px" }}
 		>
 			<h1>So good to be back coding :)</h1>
+			<Dropdown
+				options={options}
+				onSelectionChange={setColor}
+				init={initColor}
+			/>
+			<Dropdown
+				options={optionsSize}
+				onSelectionChange={setSize}
+				title='Size'
+			/>
+			<hr />
 			<Search />
 			<hr />
 			<br />
