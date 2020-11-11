2ea09fe5929d2b4a314cac51f0e7596cea0fe523
paulmboyce <paul.boyce@goobito.com>
2020-11-11 14:07:29 -0300

    docs(translate): add API and KEY documentation/links

--- src/apis/Convert.js
+++ src/apis/Convert.js
@@ -1,14 +1,19 @@
 import Axios from "axios";
 
 const Convert = async (text, language) => {
+	// SEE: https://cloud.google.com/translate/docs/reference/rest/v2/translate
 	return Axios.post(
 		"https://translation.googleapis.com/language/translate/v2",
 		{},
 		{
 			params: {
 				q: text,
+				// SEE: https://cloud.google.com/translate/docs/languages
 				target: language,
 				format: "text",
+				// NOTE: key from Stephen Grider udemy course
+				// (https://www.udemy.com/course/react-redux/learn/lecture/20788918#overview)
+				// (only works for localhost:3000)
 				key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
 			},
 		}
